2efc290f5a95f007e86ba471e59b144f0ce6aaa1
vaanessamota <vanessaoliveira2706@gmail.com>
2023-09-01 18:55:17 -0300

    feat: add base app contract and register routers

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,40 @@
+import 'package:base_app/home.dart';
 import 'package:flutter/material.dart';
 import 'package:login_micro_app/login_micro_app.dart';
+import 'package:micro_core/core/base_app.dart';
+import 'package:micro_core/core/micro_app.dart';
+import 'package:micro_core/core/micro_core_utils.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget with BaseApp {
+  MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    super.registerRouters();
+
     return MaterialApp(
       title: 'Base App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(title: 'Flutter Base App'),
-      routes: {
-        '/login': (context) => const LoginPage(),
-      },
+      navigatorKey: navigatorKey,
+      home: const HomePage(),
+      onGenerateRoute: super.generateRoute,
+      initialRoute: '/home',
     );
   }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
+  Map<String, WidgetBuilderArgs>? get baseRoutes => {
+        '/home': (context, args) => const HomePage(),
+      };
 
-class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Click the button to navigate to the Login Page',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/login');
-        },
-        tooltip: 'LoginPage',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  List<MicroApp>? get microApps => [
+        LoginMicroAppResolver(),
+      ];
 }
