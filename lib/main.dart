52cd6034a3d3619d6ea008d634a8b29a18f2de61
vaanessamota <vanessaoliveira2706@gmail.com>
2023-09-03 15:58:00 -0300

    refactor: split my app and main into different files

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,8 @@
-import 'package:base_app/home.dart';
+import 'package:base_app/myapp.dart';
 import 'package:flutter/material.dart';
-import 'package:login_micro_app/login_micro_app.dart';
-import 'package:micro_core/micro_core.dart';
-import 'package:micro_core/utils/app_locator.dart';
 
 void main() {
   final myapp = MyApp();
   myapp.configureDependencies();
   runApp(myapp);
-}
-
-class MyApp extends StatelessWidget with BaseApp {
-  MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    super.registerRouters();
-
-    return MaterialApp(
-      title: 'Base App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      navigatorKey: navigatorKey,
-      home: const HomePage(),
-      onGenerateRoute: super.generateRoute,
-      initialRoute: '/home',
-    );
-  }
-
-  @override
-  Map<String, WidgetBuilderArgs>? get baseRoutes => {
-        '/home': (context, args) => const HomePage(),
-      };
-
-  @override
-  List<MicroApp>? get microApps => [
-        LoginMicroAppResolver(),
-      ];
-
-  void configureDependencies() {
-    final locator = AppLocator();
-    MicroCoreDependencies.setUp(locator);
-    LoginMicroAppDependencies.setUp(locator);
-  }
 }
---
+++ lib/myapp.dart
@@ -0,0 +1,42 @@
+import 'package:base_app/home.dart';
+import 'package:flutter/material.dart';
+import 'package:login_micro_app/app/login_micro_app_dependencies.dart';
+import 'package:login_micro_app/app/login_micro_app_resolver.dart';
+import 'package:micro_core/micro_core.dart';
+import 'package:micro_core/utils/app_locator.dart';
+
+class MyApp extends StatelessWidget with BaseApp {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    super.registerRouters();
+
+    return MaterialApp(
+      title: 'Base App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      navigatorKey: navigatorKey,
+      home: const HomePage(),
+      onGenerateRoute: super.generateRoute,
+      initialRoute: '/home',
+    );
+  }
+
+  @override
+  Map<String, WidgetBuilderArgs>? get baseRoutes => {
+        '/home': (context, args) => const HomePage(),
+      };
+
+  @override
+  List<MicroApp>? get microApps => [
+        LoginMicroAppResolver(),
+      ];
+
+  void configureDependencies() {
+    final locator = AppLocator();
+    MicroCoreDependencies.setUp(locator);
+    LoginMicroAppDependencies.setUp(locator);
+  }
+}
